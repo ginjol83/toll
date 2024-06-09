b110a9ea799a0d45145cd8c52988342c190ed8c5
unknown <andresgime@gmail.com>
2024-06-09 23:06:02 +0200

    refactor data service

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import { login } from "./src/services/loginService.js";
 import { config } from "./src/config/config.js";
+import { dataScrapp } from "./src/services/dataService.js";
 
 const jsonLogged = login(config.url, config.user, config.pass);
 
 jsonLogged.then((data) => {
-    console.log(data);
+    dataScrapp(JSON.parse(data), config);
+  
+
 });
--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,8 +1,9 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const cheerio = require('cheerio');
+import puppeteer from 'puppeteer'
 
-const login = async (url, username, password) => {
+const dataScrapp = async (jsonData, config) => {
+
+    const { url, username, password } = config;
+
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
@@ -22,44 +23,23 @@ const login = async (url, username, password) => {
 
     // Obteniendo todo el HTML de la página
     const html = await page. content();
-    
-    await browser.close();
 
 
-    const $ = cheerio.load(html);
-    const table = $('table#example');
-    const rows = [];
-
-    table.find('tbody tr').each((i, row) => {
-        const cells = [];
-        $(row).find('td').each((j, cell) => {
-            if (j === 4 || j === 5 || j === 6 || j === 7 ) { // Si es la celda "Turnos"
-                const link = $(cell).find('a').attr('href');
-                cells.push(link);
-            } else {
-                cells.push($(cell).text().trim());
-            }
-        });
-                rows.push({
-            'Cod.Contador': cells[0],
-            'Parcela': cells[1],
-            'Superficie (Hg)': cells[2],
-            'Cultivo': cells[3],
-            'Turnos': cells[4],
-            'Inf.Caudal': cells[5],
-            'Estado': cells[6],
-            'Localización': cells[7]
-        });
-    });
+    //TODO obtener los datos de cada sección y montar el JSON de respuesta
+    console.log('jsonData', jsonData);
+    /*
+    jsonData.map(result => {
+        console.log(result.Estado);
+    })
+    */
 
 
-
-console.log(JSON.stringify(rows, null, 2));
-
-
+    await browser.close();    
 
 
 }
+
+export { dataScrapp }
 
 
 
